c29209a3ed7f7761308a95baf9a5d9fa230d70af
Hassan <muhammad.hassan@antematter.io>
2026-07-07 15:34:15 +0500

    using dedicated RPC

--- utils/get-rpc.ts
+++ utils/get-rpc.ts
@@ -6,5 +6,6 @@ import {
 
 
 export function getConnection(network: Cluster) {
-    return new Connection(clusterApiUrl(network), "confirmed");
+    return new Connection("https://devnet.helius-rpc.com/?api-key=8d46c96c-eca3-48a4-a340-9fcaf733f0fe", "confirmed");
+    // return new Connection(clusterApiUrl(network), "confirmed");
 }
